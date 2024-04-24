5626a7ffd79e81fb1d8766de17d9f57ab2912895
riteshth1 <riteshbill14@gmail.com>
2024-04-24 14:38:51 +0545

    Updated

--- Character.java
+++ Character.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Character {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) 
+    {
+        char ch;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a character: ");
+        ch = s.next().charAt(0);
+        try{
+            if(!Character.isLetter(ch))
+            {
+                throw new Exception("Please enter characters");
+            }
+            System.out.println("Your input is " +ch);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
